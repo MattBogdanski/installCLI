789711dc2a154d910a9d6c9dbab820d183b23764
MattBogdanski <43410102+MattBogdanski@users.noreply.github.com>
2019-03-26 10:17:10 +0100

    Update setUpCLI.sh
    Small changes

--- setUpCLI.sh
+++ setUpCLI.sh
@@ -10,11 +10,11 @@ sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
      --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
 
 
-read -p "You need to update all apps, if you want to exlcude some of them just finish here. In other way just update all." -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+#read -p "You need to update all apps, if you want to exlcude some of them just finish here. In other way just update all." -n 1 -r
+#echo
+#if [[ $REPLY =~ ^[Yy]$ ]]
+#then
 	sudo apt-get update
 	sudo apt-get install azure-cli
-fi
+#fi
 
